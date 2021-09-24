996a3e9b1d105730076fafd8c5cfe8104be3dccf
Zonayed Ahamad <86493532+Zonayed320@users.noreply.github.com>
2021-09-25 03:54:32 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$('body').bind("contextmenu",function(e){
+    alert(' Use Your Brain.. This Example Protected by Zonayed Ahamad')
+    return false;
+});
+
+$('.make').click(function(){
+    $(this).slideUp().hide();
+    $('.break').slideDown(1000);
+});
+$('.break').click(function(){
+    $(this).slideUp().hide();
+    $('.make').slideDown(1000);
+});
